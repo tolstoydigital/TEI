8072973b5875fb3fac969d460ab7558fbf2fc18a
Lev Bulanov <levabumail@gmail.com>
2023-01-09 21:55:12 +0200

    update update_tei_headers.py script

--- utils/update_tei_headers.py
+++ utils/update_tei_headers.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from math import isnan
 import os
 from pathlib import Path
 import re
@@ -9,7 +8,6 @@ import numpy as np
 import pandas as pd
 
 import utils as ut
-
 
 BASE_PATH = ut.REPO_PATH
 SOURCE_PATH = Path(BASE_PATH, 'texts')
@@ -37,40 +35,32 @@ def prepare_catrefs_for_comments(categories: str) -> str:
     return '\n'.join(cat_refs)
 
 
-def process_comments(row: dict, data: dict):
+def process_comments(row: dict, data: dict) -> None:
     data['comments_type_E'] = prepare_catrefs_for_comments(row['категория комментария'])
     title = data['title90_H']
     title = title.replace('<', '&lt;')
     title = title.replace('>', '&gt;')
     data['title90_H'] = title
-    # print(title)
 
 
-def process_diaries(row: dict, data: dict):
-    # date_H = ''
+def process_diaries(row: dict, data: dict) -> None:
     first_date = row['first_date']
     last_date = row['last_date']
-    # print(row['first_date'], row['last_date'])
     if isinstance(first_date, datetime):
         first_date = first_date.strftime('%Y-%m-%d')
     if isinstance(last_date, datetime):
         last_date = last_date.strftime('%Y-%m-%d')
     if row['Техническая колонка'] == '1':
         date_H = f'<date when="{first_date}"/>'
-    # elif row['Техническая колонка'] == '0':
     else:
         date_H = f'<date from="{first_date}" to="{last_date}"/>'
-    # print(first_date, last_date, row)
     if ':' in first_date:
         not_before, not_after = first_date.split(':')
         date_H = f'<date notBefore="{not_before}" notAfter="{not_after}"/>'
-    # print(row)
-    # print(row['Техническая колонка'])
     data['date_H'] = date_H
-    # print(date_H)
 
 
-def process_krug(row: dict, data: dict):
+def process_krug(row: dict, data: dict) -> None:
     months = {
         '1': 'января',
         '2': 'февраля',
@@ -93,25 +83,26 @@ def process_krug(row: dict, data: dict):
 
 
 def process_letters(row: dict, data: dict):
-    # print(row)
     # make title
     title = f'{row["адресат"]} {row["humane_readable_dates"]}'
     if row["раздел 90-томника"] == 'Письма':
         title = f'Письмо {title}'
     title = title.replace('&', '&amp;')
     data['title_V'] = title
+
     #
     data['first_publ_J'] = 'Публикуется впервые' if isinstance(row['звездочка (опубликовано впервые)'], str) else ''
+
     # date
     data['date_OPQ'] = f'<date when="{row["when"]}"/>' if isinstance(
         row["when"], str) else f'<date notBefore="{row["notBefore"]}" notAfter="{row["notAfter"]}"/>'
+
     # addressees
     if not isinstance(row["Адресат - берем из xml  (берем из колонки H из адресатов)"], str):
         data['pers_name_TU'] = '<persName/>'
     elif not isinstance(row["ID Адресата - вытаскиваем из таблицы Бори"], str):
         data['pers_name_TU'] = '<persName/>'
     else:
-        # print(row["Адресат - берем из xml  (берем из колонки H из адресатов)"], str)
         names = row["Адресат - берем из xml  (берем из колонки H из адресатов)"].split('|')
         names_ids = row["ID Адресата - вытаскиваем из таблицы Бори"].split('|')
         pairs = [(n, n_id) for n, n_id in zip(names, names_ids)]
@@ -120,13 +111,15 @@ def process_letters(row: dict, data: dict):
             tags.append(f'<persName ref="{name_id}">{name}</persName>')
         indent = '\n' + ' ' * 20
         data['pers_name_TU'] = indent.join(tags)
+
     # language
     data['language_Z'] = 'ru'  # row["язык"]
+
     #
     data['sent_E'] = 'sent' if row['статус (отпр, неотпр, черн)'] is None else 'not_sent'
 
 
-def process_notes(row: dict, data: dict):
+def process_notes(row: dict, data: dict) -> None:
     # date_H
     if row['Техническая колонка'] == '1':
         date_H = f'<date when="{row["date"]}"/>'
@@ -136,13 +129,10 @@ def process_notes(row: dict, data: dict):
     data['date_H'] = date_H
 
 
-def process_works(row: dict, data: dict):
+def process_works(row: dict, data: dict) -> None:
     # language
     language = row['language']
     data['language_K'] = 'ru' if language is None else ut.convert_language_to_iso(language)
-    # publ
-    # data['published_AY'] = 'publ' if row['Published TEI'] == 'publ' else 'not_publ'
-    # data['published_AY'] = row['(not) published SIMPLE ']
 
     for column, value in data.items():
         try:
@@ -159,7 +149,6 @@ def process_works(row: dict, data: dict):
     # drugoe
     if data['genre_drugoe_AG'] is not None and data['genre_drugoe_AG'].strip():
         data['genre_drugoe_AG'] = 'drugoe'
-#             'genre_drugoe_AG': 'Другое (жанр)'
 
 
 FOLDERS_DATA = {
@@ -171,7 +160,7 @@ FOLDERS_DATA = {
             'title_K': 'name',
             'subtitle_M': 'дополнение к названию',
             'title_as_in_90_B': 'название как в 90 томнике',
-            'title_level_s_F': 'book',  # there are some without
+            'title_level_s_F': 'book',
             'start_page_D': 'start page',
             'end_page_E': 'end page',
             'date_N': 'Год создания (Тим)',
@@ -211,7 +200,6 @@ FOLDERS_DATA = {
             'date_L': 'Год тома',
             'start_page_E': 'first_page',
             'end_page_F': 'last_page',
-            # 'date_H': 'first_date',
             'bibliography': 'БИБЛ = итог'
         }
     },
@@ -227,7 +215,6 @@ FOLDERS_DATA = {
             'start_page_D': 'start page',
             'end_page_E': 'end page',
             'date_O': 'Год создания (Тим)',
-            # 'author': row[10].strip(),
             'sphere_S': 'fiction/nonfiction - Фекла',
             'finished_T': 'finished/not finished - Лева',
             'published_U': 'published/not published - Лева',
@@ -288,7 +275,7 @@ FOLDERS_DATA = {
             'published_date_AL': 'Year of  volume',
             'main_AH': '  MAIN',
             'genre_drugoe_AG': 'Другое (жанр)',
-            'bibliography': 'БИБЛ = итог'
+            'bibliography': 'БИБЛ = итог + страницы = cured'
         }
     },
 }
@@ -301,9 +288,8 @@ def normalize_row(row: dict) -> dict:
     return row
 
 
-def fix_date_tag(root: etree._Element):
+def fix_date_tag(root: etree.Element) -> None:
     header_tag = root.xpath('//ns:teiHeader', namespaces={'ns': XMLNS})[0]
-    # etree.indent(header_tag)
     date_tags = [tag for tag in root.xpath('//ns:date', namespaces={'ns': XMLNS})
                  if tag in header_tag.iterdescendants()]
     for tag in date_tags:
@@ -314,14 +300,13 @@ def fix_date_tag(root: etree._Element):
         if single_date_match is not None:
             tag.set('when', single_date_match.group(1))
             tag.text = None
-            # print(single_date_match.group(1), etree.tostring(tag, encoding='unicode'))
         elif range_match is not None:
             tag.set('from', range_match.group(1))
             tag.set('to', range_match.group(3))
             tag.text = None
 
 
-def remove_empty_catref(root: etree._Element):
+def remove_empty_catref(root: etree.Element) -> None:
     cat_refs = root.xpath(f'//ns:catRef', namespaces={'ns': XMLNS})
     for tag in cat_refs:
         value = tag.get('ana')
@@ -329,31 +314,28 @@ def remove_empty_catref(root: etree._Element):
             tag.getparent().remove(tag)
 
 
-def remove_header_tags_with_none_text(root: etree._Element):
-    """If tag has text 'None' (not `is None`!), remove the tag."""
-    # title_tags = root.xpath('//ns:title', namespaces={'ns': XMLNS})
+def remove_header_tags_with_none_text(root: etree.Element) -> None:
+    """If tag has text 'None' (not {is None}!), remove the tag."""
     header_tag = root.xpath('//ns:teiHeader', namespaces={'ns': XMLNS})[0]
-    # for tag in title_tags:
     for tag in header_tag.iterdescendants():
         if tag.text is not None and tag.text.strip() == 'None':
             tag.getparent().remove(tag)
 
 
-def indent_header_tag(root: etree._Element):
+def indent_header_tag(root: etree.Element) -> None:
     header_tag = root.xpath('//ns:teiHeader', namespaces={'ns': XMLNS})[0]
     etree.indent(header_tag)
 
 
-def restore_p_uuid_in_header(root, path_to_folder, filename):
+def restore_p_uuid_in_header(root: etree.Element, path_to_folder: Path, filename: str) -> None:
     old_root = etree.fromstring(ut.read_xml(Path(SOURCE_PATH, Path(path_to_folder).name, filename), 'rb'))
     old_p_tag = old_root.xpath('//ns:availability//ns:p', namespaces={'ns': XMLNS})[0]
-    # print(old_p_tag.attrib)
     old_id = old_p_tag.attrib['id']
     p_tag = root.xpath('//ns:availability//ns:p', namespaces={'ns': XMLNS})[0]
     p_tag.set('id', old_id)
 
 
-def add_resp_to_title(root: etree._Element, row: dict):
+def add_resp_to_title(root: etree.Element, row: dict) -> None:
     title_big = root.xpath('//ns:title[@xml:id]', namespaces={'ns': XMLNS})[0]
     title_main = root.xpath('//ns:title[@type="main"]', namespaces={'ns': XMLNS})[0]
     if row['название дано редакторами (сверять по списку 91 тома)'] is not None and \
@@ -375,25 +357,39 @@ def fix_special_entities(text: str) -> str:
     return text
 
 
+def add_old_orthographyh_tag(root: etree.Element) -> None:
+    # Если есть orig, то добавляем
+    orig_tags = root.xpath('//ns:orig', namespaces={'ns': XMLNS})
+    if not orig_tags:
+        return
+    try:
+        lang_tag = root.xpath('//ns:langUsage', namespaces={'ns': XMLNS})[0]
+    except IndexError:
+        text_class_tag = root.xpath('//ns:textClass', namespaces={'ns': XMLNS})[0]
+        lang_tag = etree.Element('langUsage')
+        text_class_tag.addprevious(lang_tag)
+    old_orthography_tag = etree.Element('language', ident='ru-petr1708')
+    old_orthography_tag.text = None
+    lang_tag.append(old_orthography_tag)
+    etree.indent(lang_tag)
+
+
 def update_folder(path_to_folder: Path, path_to_sheet: Path, path_to_header_template: Path) -> None:
     foldername = path_to_folder.name
     df = pd.read_excel(path_to_sheet, sheet_name=FOLDERS_DATA[foldername]['sheetname'],
                        header=1 if foldername == 'works' else 0)
     df = df.replace(np.nan, None)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('display.width', 1000)
-    # print(df.head())
+    # pd.set_option('display.max_columns', 500)
+    # pd.set_option('display.width', 1000)
+
     column_names = FOLDERS_DATA[foldername]['column_names']
-    # print(column_names)
     template = path_to_header_template.read_text()
     for filename in os.listdir(path_to_folder):
         file_id = filename.replace('.xml', '')
         if file_id == 'test-front_new':
             continue
-        # print(file_id)
         row = df.loc[df[column_names['id']] == file_id].to_dict('records')[0]
         row = normalize_row(row)
-        # print(row)
         data = {mapping: row[column] for mapping, column in column_names.items()}
         data['bibliography'] = fix_special_entities(data['bibliography'])
         try:
@@ -404,10 +400,8 @@ def update_folder(path_to_folder: Path, path_to_sheet: Path, path_to_header_temp
         root = etree.fromstring(ut.read_xml(f'{path_to_folder}/{filename}', 'rb'))
         text_tag = root.xpath('//ns:text', namespaces={'ns': XMLNS})[0]
         text = etree.tostring(text_tag, encoding='unicode').strip('\n')
-        # print(data)
         data['text'] = text
         result_text = template.format(**data)
-        # print(result_text)
 
         root = etree.fromstring(result_text.encode())
 
@@ -417,13 +411,14 @@ def update_folder(path_to_folder: Path, path_to_sheet: Path, path_to_header_temp
         if foldername == 'works':
             add_resp_to_title(root, row)
         restore_p_uuid_in_header(root, path_to_folder, filename)
+        add_old_orthographyh_tag(root)
         result_text = etree.tostring(root, encoding='unicode').strip('\n')
         result_text = f"<?xml version='1.0' encoding='UTF-8'?>\n{result_text}"
 
-        check_root = etree.fromstring(result_text.encode())
+        # check that xml is valid
+        etree.fromstring(result_text.encode())
 
         Path(RESULT_PATH, foldername, filename).write_text(result_text)
-    pass
 
 
 def main():
@@ -434,7 +429,6 @@ def main():
     update_folder(Path(SOURCE_PATH, 'letters'), LETTERS_SHEET, LETTERS_HEADER)
     update_folder(Path(SOURCE_PATH, 'notes'), WORKS_SHEET, NOTES_HEADER)
     update_folder(Path(SOURCE_PATH, 'works'), WORKS_SHEET, WORKS_HEADER)
-    pass
 
 
 if __name__ == '__main__':
