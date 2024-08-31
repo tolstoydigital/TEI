184623b8a1ec4dd4f5a3e031eda6567cb6080149
Konstantin Petrov <konstantinpetrovtf@gmail.com>
2024-08-31 19:17:27 +0300

    feat(tolstoy-bio/gusev): add scripts that convert record stubs to TEI

--- tolstoy-bio/tolstoy_bio/gusev/scripts/converter/fragment_processor.py
+++ tolstoy-bio/tolstoy_bio/gusev/scripts/converter/fragment_processor.py
@@ -3,15 +3,17 @@ import os
 from tolstoy_bio.utilities.beautiful_soup import BeautifulSoupUtils
 from .record import Record
 from .record_processor import RecordProcessor
+from .record_id_manager import RecordIdManager
 
 
 TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "template.xml")
 
 
 class FragmentProcessor:
-    def __init__(self, source_path: str):
+    def __init__(self, source_path: str, record_id_manager: RecordIdManager):
         self.source_path = source_path
         self.source_soup = BeautifulSoupUtils.create_soup_from_file(source_path, "xml")
+        self.record_id_manager = record_id_manager
 
     def convert_records_to_tei(self, saving_repository_path: str):
         records_elements = self.source_soup.find_all("_Records")
@@ -23,7 +25,7 @@ class FragmentProcessor:
 
         for record in records_entities:
             record_processor = RecordProcessor(record)
-            record_processor.convert_to_tei()
-            
-            saving_path = os.path.join(saving_repository_path, f"{record_processor.get_record_id()}.xml")
-            record_processor.format_and_save(saving_path)
+            record_id = record_processor.generate_id()
+            record_filename = self.record_id_manager.generate_based_on(record_id)
+            saving_path = os.path.join(saving_repository_path, f"{record_filename}.xml")
+            record_processor.convert_to_tei(saving_path)
--- tolstoy-bio/tolstoy_bio/gusev/scripts/converter/main.py
+++ tolstoy-bio/tolstoy_bio/gusev/scripts/converter/main.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 
 from tolstoy_bio.utilities.io import IoUtils
 from .fragment_processor import FragmentProcessor
+from .record_id_manager import RecordIdManager
 
 
 SOURCE_REPOSITORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../data/source")
@@ -11,10 +12,11 @@ TEI_REPOSITORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..
 
 def main():
     fragments_paths = IoUtils.get_folder_contents_paths(SOURCE_REPOSITORY, ignore_hidden=True)
+    record_id_manager = RecordIdManager()
 
     for fragment_path in tqdm(sorted(fragments_paths)):
         try:
-            fragment_processor = FragmentProcessor(fragment_path)
+            fragment_processor = FragmentProcessor(fragment_path, record_id_manager)
             fragment_processor.convert_records_to_tei(TEI_REPOSITORY)
         except Exception as e:
             print(fragment_path)
