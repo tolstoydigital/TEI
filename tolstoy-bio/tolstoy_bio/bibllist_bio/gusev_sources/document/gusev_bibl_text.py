c7da48b4d0bcc7c5ac0bee19530320addf8135e4
Konstantin Petrov <konstantinpetrovtf@gmail.com>
2025-02-06 21:12:24 +0300

    feat(tolstoy-bio): generate non-tolstoy-bio bibl segments list

--- tolstoy-bio/tolstoy_bio/bibllist_bio/gusev_sources/document/gusev_bibl_text.py
+++ tolstoy-bio/tolstoy_bio/bibllist_bio/gusev_sources/document/gusev_bibl_text.py
@@ -7,6 +7,9 @@ from .gusev_bibl_text_segment import GusevBiblTextSegment
 class GusevBiblText:
     text: str
 
+    def to_string(self) -> str:
+        return self.text.strip()
+
     def get_segments(self) -> list[GusevBiblTextSegment]:
         segments = self.text.split(";")
         stripped_segments = [segment.strip() for segment in segments]
